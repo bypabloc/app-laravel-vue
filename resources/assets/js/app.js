d9a7724958ef39871f1d0ca1ddad51284c320d1e
bypabloc <pacg1991@gmail.com>
2021-11-07 13:12:48 -0500

    Update app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,6 +5,11 @@ import "bootstrap"
 
 import Index from  './views/Index.vue'
 
+const MIX_TEST_1 = process.env.MIX_TEST_1 ?? '';
+const MIX_TEST_2 = process.env.MIX_TEST_2 ?? '';
+console.log('MIX_TEST_1',MIX_TEST_1)
+console.log('MIX_TEST_2',MIX_TEST_2)
+
 const app = createApp({
     components: {
         Index,
