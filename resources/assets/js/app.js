22ad3e779e31b36aec28d813b369a7801cc4d373
bypabloc <pacg1991@gmail.com>
2021-11-07 18:07:06 -0500

    Update app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,16 +5,16 @@ import "bootstrap"
 
 import Index from  './views/Index.vue'
 
-const MIX_APP_URL = process.env.MIX_APP_URL ?? 'MIX_APP_URL nada por aca';
+const MIX_APP_URL = process.env.MIX_APP_URL ?? 'nada por aca';
 console.log('MIX_APP_URL',MIX_APP_URL)
 
-const MIX_TEST_1 = process.env.MIX_TEST_1 ?? 'MIX_TEST_1 nada por aca';
+const MIX_TEST_1 = process.env.MIX_TEST_1 ?? 'nada por aca';
 console.log('MIX_TEST_1',MIX_TEST_1)
 
-const MIX_TEST_2 = process.env.MIX_TEST_2 ?? 'MIX_TEST_2 nada por aca';
+const MIX_TEST_2 = process.env.MIX_TEST_2 ?? 'nada por aca';
 console.log('MIX_TEST_2',MIX_TEST_2)
 
-const MIX_TEST_3 = process.env.MIX_TEST_3 ?? 'MIX_TEST_3 nada por aca';
+const MIX_TEST_3 = process.env.MIX_TEST_3 ?? 'nada por aca';
 console.log('MIX_TEST_3',MIX_TEST_3)
 
 const app = createApp({
