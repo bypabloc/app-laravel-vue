e20807cb126fbd08b04c9ec1fc22277513474875
bypabloc <pacg1991@gmail.com>
2021-11-07 14:26:18 -0500

    Update app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,12 +8,10 @@ import Index from  './views/Index.vue'
 const MIX_TEST_1 = process.env.MIX_TEST_1 ?? 'MIX_TEST_1 nada por aca';
 const MIX_TEST_2 = process.env.MIX_TEST_2 ?? 'MIX_TEST_2 nada por aca';
 const MIX_TEST_3 = process.env.MIX_TEST_3 ?? 'MIX_TEST_3 nada por aca';
-const MIX_TEST_4 = process.env.MIX_TEST_4 ?? 'MIX_TEST_4 nada por aca';
 
 console.log('MIX_TEST_1',MIX_TEST_1)
 console.log('MIX_TEST_2',MIX_TEST_2)
 console.log('MIX_TEST_3',MIX_TEST_3)
-console.log('MIX_TEST_4',MIX_TEST_4)
 
 const app = createApp({
     components: {
