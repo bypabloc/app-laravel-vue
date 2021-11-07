32665e78d50ee5d6ffd6d80b666f6c12241a92f7
bypabloc <pacg1991@gmail.com>
2021-11-07 17:18:51 -0500

    package.json add script heroku

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,14 @@
 const mix = require('laravel-mix');
+require('dotenv').config();
+const path = require('path');
 
 // Get the APP_URL from .env and remove protocol
 let url = process.env.APP_URL.replace(/(^\w+:|^)\/\//, '');
 
+console.log('APP_KEY',process.env.APP_KEY)
+
 if (mix.inProduction()) {
-   mix.version();
+    mix.version();
 }else{
     mix.options({
         hmrOptions: {
@@ -13,6 +17,16 @@ if (mix.inProduction()) {
         }
     });
 }
+
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            '@': path.join(__dirname, 'resources/assets/js'),
+            '~': '/resources/assets/sass',
+            '@components': '/resources/assets/js/components',
+        }
+    }
+});
 
 mix.js('resources/assets/js/app.js', 'public/js')
 // .sass('resources/assets/sass/app.scss', 'public/css')
